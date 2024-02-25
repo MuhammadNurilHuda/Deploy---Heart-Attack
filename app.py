e17624dff8a6b2f20c5605164eec8a382181d7e1
MuhammadNurilHuda <nurilhuda3333@gmail.com>
2024-02-26 00:38:17 +0700

    minor update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import pickle
 import numpy as np
 
@@ -9,23 +9,31 @@ scaler = pickle.load(open('minmax_scaler.pkl', 'rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
+    # return jsonify({'Message':'Hello, World!'})
 
-@app.route( '/predict', methods=['GET', 'POST'])
+@app.route( '/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
+    try:
         feature = ['age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg', 'thalachh','exng', 'oldpeak', 'slp', 'caa', 'thall']
         data = [float(request.form[f]) for f in feature]
         data_array = np.array([data]).reshape(1, -1)
         data_normalized = scaler.fit_transform(data_array)
         prediction = model.predict(data_normalized)[0]
 
-        if int(prediction) == 1:
-            prediction = 'You have more chance of heart attack'
+        if prediction == 1:
+            msg = 'You have more chance of heart attack'
         else:
-            prediction = 'No heart attack expected'
-        return render_template('index.html', prediction = prediction)
+            msg = 'No heart attack expected'
+
+        response = {
+            'prediction':msg
+        }
+        
+        return render_template('index.html', prediction = msg)
+        # return jsonify(response)
     
-    return render_template('index.html', prediction = '')
+    except Exception as e:
+        return jsonify({'error': str(e)})
     
 if __name__ == "__main__":
     app.run(debug=True)
